39421817c5a1ada28a7f7c2e9efd45125faa521e
Stewart Alsop <stewartalsopIII@gmail.com>
2015-02-17 19:50:57 -0600

    git is confusing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(params.require(:comment).permit(:body))
-    @comment.user = current_user #CHANGE WITH AUTHENTICATION
+    @comment.user = current_user 
 
     if @comment.save
       flash[:notice] = "Comment was saved"
